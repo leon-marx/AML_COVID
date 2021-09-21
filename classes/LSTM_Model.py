32abb980808df05718b2b38e4b1e7d7bad6920df
leon-marx <gooogleon@gmail.com>
2021-09-21 21:05:44 +0200

    Changed the way the model trains.

--- classes/LSTM_Model.py
+++ classes/LSTM_Model.py
@@ -8,29 +8,27 @@ device = "cuda" if torch.cuda.is_available() else "cpu"
 
 class LSTM(nn.Module):
 
-    def __init__(self, input_size, hidden_size, output_size, num_layers, dropout):
+    def __init__(self, input_size, hidden_size, num_layers, dropout):
         """
         input_size: number of features in input, generally 1
         hidden_size: number of hidden neurons, e.g. 256
-        output_size: how many days to predict, generally 1
         num_layers: number of stacked LSTMs, generally 1
         dropout: dropout-probability, 0 corresponds to no dropout
         """
         super(LSTM, self).__init__()
         self.input_size = input_size + 5
         self.hidden_size = hidden_size
-        self.output_size = output_size
         self.num_layers = num_layers
         self.dropout = dropout
         self.lstm = nn.LSTM(input_size=self.input_size,
                           hidden_size=self.hidden_size,
                           num_layers=self.num_layers,
                           dropout=self.dropout)
-        self.linear = nn.Linear(in_features=self.hidden_size, out_features=self.output_size)
+        self.linear = nn.Linear(in_features=self.hidden_size, out_features=1)
         self.get_h0 = nn.Linear(in_features=5, out_features=self.num_layers * self.hidden_size)
         self.get_c0 = nn.Linear(in_features=5, out_features=self.num_layers * self.hidden_size)
 
-    def forward(self, sequence, inits=None):
+    def forward(self, sequence, inits=None, full=False):
         """
         sequence: should be a time series of shape (L, N, input_PP_size) with:
             L: sequence length, e.g. 50 days
@@ -50,9 +48,45 @@ class LSTM(nn.Module):
             inits = (h_0, c_0)
         sequence = sequence.to(device)
         output, (h_final, c_final) = self.lstm(sequence, inits)
-        output = self.linear(output[-1]).view(1, N, self.output_size)
+        output = self.linear(output[-1]).view(1, N, 1)
+        if full:
+            return output, (h_final, c_final)
         return output
 
+    def forward_long(self, sequence, PP_input, n_days):
+        """
+        sequence: should be a time series of shape (L, N, input_size) with:
+            L: sequence length, e.g. 50 days
+            N: batch_size, generally 1
+            input_size: as above, generally 1
+        PP_input: pandemic parameters, tensor of shape (L, N, 5) with:
+            L: sequence length, e.g. 50 days
+            N: batch_size
+            5: the 5 different PP-values:
+                N_pop: population size
+                D: average degree of social network in population
+                r: daily transmission rate between two individuals which were in contact
+                d: duration of the infection
+                epsilon: rate of cross-contacts
+        n_days: number of days to predict in the future
+        """
+        sequence = sequence.to(device)
+        PP_input = PP_input.to(device)
+        preds = torch.zeros(size=(n_days, sequence.shape[1], self.input_size-5))
+        for i in range(n_days):
+            PP_sequence = torch.cat((sequence, PP_input), dim=2) #19,1,6 (number of days, ..., 6 dimensions [5PP,1value]); 10,1,6
+            if i == 0:
+                h_0 = self.get_h0(PP_input[0]).view(self.num_layers, PP_input.shape[1], self.hidden_size) #2,1,256
+                c_0 = self.get_c0(PP_input[0]).view(self.num_layers, PP_input.shape[1], self.hidden_size) #2,1,256
+                pred, (h_final, c_final) = self.forward(PP_sequence, inits=(h_0, c_0), full=True)
+                new_PP_seq = torch.cat((pred, PP_input[-1].view(1, PP_input.shape[1], 5)), dim=2)
+                preds[i] = pred
+            else:
+                pred, (h_final, c_final)= self.forward(new_PP_seq, inits=(h_final, c_final), full=True)
+                new_PP_seq = torch.cat((pred, PP_input[-1].view(1, PP_input.shape[1], 5)), dim=2)
+                preds[i] = pred
+        return preds
+    
     def predict(self, sequence, PP_input):
         """
         sequence: should be a time series of shape (L, N, input_size) with:
@@ -95,23 +129,26 @@ class LSTM(nn.Module):
                 epsilon: rate of cross-contacts
         n_days: number of days to predict in the future
         """
-        preds = []
-        L = sequence.shape[0] # 19,1,1
+        sequence = sequence.to(device)
+        PP_input = PP_input.to(device)
+        preds = torch.zeros(size=(n_days, sequence.shape[1], self.input_size-5), requires_grad=True)
         for i in range(n_days):
             PP_sequence = torch.cat((sequence, PP_input), dim=2) #19,1,6 (number of days, ..., 6 dimensions [5PP,1value]); 10,1,6
             self.eval()
             with torch.no_grad():
-                # Compute prediction error
-                h_0 = self.get_h0(PP_input[0]).view(self.num_layers, PP_input.shape[1], self.hidden_size) #2,1,256
-                c_0 = self.get_c0(PP_input[0]).view(self.num_layers, PP_input.shape[1], self.hidden_size) #2,1,256
-                pred = self.forward(PP_sequence, inits=(h_0, c_0))
-                preds.append(pred) #1,1,1
-                sequence = torch.cat((sequence, pred), dim=0)[-n_days:]#19,1,1 -> 20,1,1 -> 10,1,1 -> select last 10 steps; basically move one step forward with the predicted value 
-                PP_input = torch.cat((PP_input, PP_input[-1].view(1, -1, 5)), dim=0)[-n_days:] # ;11,1,5 -> 10,1,5
-                #TODO append prediction for PP_input or do they remain the same?
-        return torch.Tensor(preds)
+                if i == 0:
+                    h_0 = self.get_h0(PP_input[0]).view(self.num_layers, PP_input.shape[1], self.hidden_size) #2,1,256
+                    c_0 = self.get_c0(PP_input[0]).view(self.num_layers, PP_input.shape[1], self.hidden_size) #2,1,256
+                    pred, (h_final, c_final) = self.forward(PP_sequence, inits=(h_0, c_0), full=True)
+                    new_PP_seq = torch.cat((pred, PP_input[-1].view(1, PP_input.shape[1], 5)), dim=2)
+                    preds[i] = pred
+                else:
+                    pred, (h_final, c_final)= self.forward(new_PP_seq, inits=(h_final, c_final), full=True)
+                    new_PP_seq = torch.cat((pred, PP_input[-1].view(1, PP_input.shape[1], 5)), dim=2)
+                    preds[i] = pred
+        return preds
 
-    def train_model(self, training_data, training_PP, loss_fn, optimizer, epoch, verbose=False):
+    def train_model_old(self, training_data, training_PP, loss_fn, optimizer, epoch=None, verbose=False):
         """
         Implements one training step for a given time-series
         training_data: should be a time series of shape (L, N, input_size) with:
@@ -136,9 +173,9 @@ class LSTM(nn.Module):
         training_PP = training_PP.to(device)
 
         # Split data 
-        X_data = training_data[:-self.output_size]
-        y_data = training_data[-self.output_size:]
-        X_PP = training_PP[:-self.output_size]
+        X_data = training_data[:-1]
+        y_data = training_data[-1:]
+        X_PP = training_PP[:-1]
 
         # Combine PP and timeseries with cumulative COVID-19 cases 
         training_seq = torch.cat((X_data, X_PP), dim=2)
@@ -148,6 +185,59 @@ class LSTM(nn.Module):
         h_0 = self.get_h0(X_PP[0]).view(self.num_layers, X_PP.shape[1], self.hidden_size)
         c_0 = self.get_c0(X_PP[0]).view(self.num_layers, X_PP.shape[1], self.hidden_size)
         pred = self.forward(training_seq, inits=(h_0, c_0))
+        loss = loss_fn(pred, y_data)
+
+        # Backpropagation
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        if verbose:
+            print(f"Epoch: {epoch} | Training Loss:", loss.item())
+
+    def train_model(self, training_data, training_PP, loss_fn, optimizer, epoch=None, verbose=False):
+        """
+        Implements one training step for a given time-series
+        training_data: should be a time series of shape (L, N, input_size) with:
+            L: sequence length, e.g. 50 days
+            N: batch_size
+            input_size: as above, generally 1
+        training_PP: pandemic parameters, tensor of shape (L, N, 5) with:
+            L: sequence length, e.g. 50 days
+            N: batch_size
+            5: the 5 different PP-values:
+                N_pop: population size
+                D: average degree of social network in population
+                r: daily transmission rate between two individuals which were in contact
+                d: duration of the infection
+                epsilon: rate of cross-contacts
+        loss_fn: use the loss functions provided by pytorch
+        optimizer: use the optimizers provided by pytorch
+        verbose: set True to print out the Training Loss
+        """
+        L = training_data.shape[0]
+        N = training_data.shape[1]
+        BACKTIME = 20
+        FORETIME = 10
+
+        # Put on GPU if possible
+        training_data = training_data.to(device)
+        training_PP = training_PP.to(device)
+        
+        # Split data into many slices
+        X_data = torch.zeros(size=(BACKTIME, N*(L-(BACKTIME+FORETIME)), self.input_size-5))
+        X_PP_data = torch.zeros(size=(BACKTIME, N*(L-(BACKTIME+FORETIME)), 5))
+        y_data = torch.zeros(size=(FORETIME, N*(L-(BACKTIME+FORETIME)), self.input_size-5))
+        for i in range(L-(BACKTIME+FORETIME)):
+            X = training_data[i:i+BACKTIME]
+            X_PP = training_PP[i:i+BACKTIME]
+            y = training_data[i+BACKTIME:i+BACKTIME+FORETIME]
+            X_data[:, i*N:(i+1)*N, :] = X
+            X_PP_data[:, i*N:(i+1)*N, :] = X_PP
+            y_data[:, i*N:(i+1)*N, :] = y
+
+        # Predict value for next timestep and compute prediction error
+        self.train()
+        pred = self.forward_long(X_data, X_PP_data, FORETIME)
         loss = loss_fn(pred, y_data)
 
         # Backpropagation
@@ -174,22 +264,31 @@ class LSTM(nn.Module):
                 epsilon: rate of cross-contacts
         loss_fn: use the loss functions provided by pytorch
         """
+        L = test_data.shape[0]
+        N = test_data.shape[1]
+        BACKTIME = 20
+        FORETIME = 10
+
+        # Put on GPU if possible
         test_data = test_data.to(device)
         test_PP = test_PP.to(device)
-
-        X_data = test_data[:-self.output_size]
-        y_data = test_data[-self.output_size:]
-        X_PP = test_PP[:-self.output_size]
-
-        test_seq = torch.cat((X_data, X_PP), dim=2)
-        test_loss = 0
+        
+        # Split data into many slices
+        X_data = torch.zeros(size=(BACKTIME, N*(L-(BACKTIME+FORETIME)), self.input_size-5))
+        X_PP_data = torch.zeros(size=(BACKTIME, N*(L-(BACKTIME+FORETIME)), 5))
+        y_data = torch.zeros(size=(FORETIME, N*(L-(BACKTIME+FORETIME)), self.input_size-5))
+        for i in range(L-(BACKTIME+FORETIME)):
+            X = test_data[i:i+BACKTIME]
+            X_PP = test_PP[i:i+BACKTIME]
+            y = test_data[i+BACKTIME:i+BACKTIME+FORETIME]
+            X_data[:, i*N:(i+1)*N, :] = X
+            X_PP_data[:, i*N:(i+1)*N, :] = X_PP
+            y_data[:, i*N:(i+1)*N, :] = y
 
         self.eval()
         with torch.no_grad():
             # Compute prediction error
-            h_0 = self.get_h0(X_PP[0]).view(self.num_layers, X_PP.shape[1], self.hidden_size)
-            c_0 = self.get_c0(X_PP[0]).view(self.num_layers, X_PP.shape[1], self.hidden_size)
-            pred = self.forward(test_seq, inits=(h_0, c_0))
+            pred = self.predict_long(X_data, X_PP_data, FORETIME)
             test_loss = loss_fn(pred, y_data).item()
         print("Average Test Loss:", test_loss)
 
@@ -286,7 +385,6 @@ if __name__ == "__main__":
 
     input_size = 1
     hidden_size = 256
-    output_size = 1
     num_layers = 2
     dropout = 0.5
 
@@ -296,7 +394,7 @@ if __name__ == "__main__":
     n_days = 5
 
 
-    MyLSTM = LSTM(input_size=input_size, hidden_size=hidden_size, output_size=output_size, num_layers=num_layers, dropout=dropout).to(device)
+    MyLSTM = LSTM(input_size=input_size, hidden_size=hidden_size, num_layers=num_layers, dropout=dropout).to(device)
 
     # Print model and its parameters
 
