d5aea7d0b57237485d29251f6456315a2f8478b6
gitexa <haas.alexanderjulian@gmail.com>
2021-09-26 03:01:51 +0200

    Updated boxplots

--- classes/create_pp_box_plots.py
+++ classes/create_pp_box_plots.py
@@ -17,8 +17,8 @@ import ast
 
 if __name__ == "__main__":
 
-    reps = 10
-    num_pp_eval = 50
+    #num_pp_eval = 50
+    cost_threshold = 0.01
 
     with open("./classes/Countries/wave_regions.json","r") as file:
         waves = json.load(file)
@@ -34,7 +34,9 @@ if __name__ == "__main__":
             
             # get optimal pps 
             df_pp = pd.read_csv(f'./data/gridsearch_results/GS_fit_{country}_{wave_id}.csv')      
-            optim_pp_list = df_pp['0'][:num_pp_eval]
+            costs = df_pp['1']
+            optim_pp_list = df_pp['0'][costs<=0.005] #[:num_pp_eval]
+            num_pp_eval = len(optim_pp_list)
 
             # delete version, smooth-transition and N 
             optim_pp_list_reduced, N, T, T_change_D = list(), list(), list(), list()
@@ -57,5 +59,5 @@ if __name__ == "__main__":
 
             plt.figure(figsize = (30,15))
             plt.boxplot(all_pps,labels=keys)
-            plt.title(f'{country} | wave {wave_id} | N = {N[0]} | Best {num_pp_eval} pp combinations')
+            plt.title(f'{country} | wave {wave_id} | N = {N[0]} | Best {num_pp_eval} pp combinations with loss <= {cost_threshold}')
             plt.savefig(f"./data/boxplots/boxplot_{country}_wave_{wave_id}_paper_average_{num_pp_eval}_initial.jpg")
